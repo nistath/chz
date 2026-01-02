f35208e6cdff4b78b1ffa29dbe66375199aced57
Nick Stathas <nickstathas@outlook.com>
2026-01-01 22:55:34 -0800

    children

--- tests/test_blueprint_mud_typing.py
+++ tests/test_blueprint_mud_typing.py
@@ -16,8 +16,34 @@ class Parent:
     value: int
 
 
+@chz.chz
+class PolyBase:
+    a: int
+
+
+@chz.chz
+class PolyChild(PolyBase):
+    b: int
+
+
+@chz.chz
+class PolyParent:
+    child: PolyBase = chz.field(
+        meta_factory=chz.factories.subclass(PolyBase, default_cls=PolyBase)
+    )
+
+
 def test_blueprint_mud_typing() -> None:
     view = chz.Blueprint(Parent).mud()
     view.value = 1
     assert_type(view.value, int)
     assert_type(view.child, Child)
+
+
+def test_blueprint_mud_polymorphic_typing() -> None:
+    child_view = chz.Blueprint(PolyParent).mud_view("child", PolyChild)
+    child_view.a = 1
+    assert_type(child_view.a, int)
+    assert_type(child_view, PolyChild)
+    child_view.b = 1
+    assert_type(child_view.b, int)
